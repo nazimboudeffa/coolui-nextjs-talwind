7f60a3ea08d00632d063f7c6db8bf534bb00971c
Nazim Boudeffa <boudeffa@gmail.com>
2024-04-06 02:12:46 +0200

    Adding more components

--- src/app/reveal/page.tsx
+++ src/app/reveal/page.tsx
@@ -3,8 +3,195 @@ import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
+    <>
+    <div className="flex flex-wrap items-center justify-center">
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.8 }}
+            transition={{ duration: 1, type: "spring", bounce: 0.7 }}
+            variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 1, y: 0 }
+            }}
+            className="m-3"
+        >
+            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
+                BOX
+            </div>
+        </motion.div>
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 2, type: "spring", bounce: 0.7 }}
+            variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 1, y: 0 }
+            }}
+            className="m-3"
+        >
+            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
+                BOX
+            </div>
+        </motion.div>
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 3, type: "spring", bounce: 0.7 }}
+            variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 1, y: 0 }
+            }}
+            className="m-3"
+        >
+            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
+                BOX
+            </div>
+        </motion.div>
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 4, type: "spring", bounce: 0.7 }}
+            variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 1, y: 0 }
+            }}
+            className="m-3"
+        >
+            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
+                BOX
+            </div>
+        </motion.div>
+    </div>
+    <div className="flex flex-wrap items-center justify-center">
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+            variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 1, y: 0 }
+            }}
+            className="m-3"
+        >
+            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
+                BOX
+            </div>
+        </motion.div>
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+            variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 1, y: 0 }
+            }}
+            className="m-3"
+        >
+            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
+                BOX
+            </div>
+        </motion.div>
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+            variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 1, y: 0 }
+            }}
+            className="m-3"
+        >
+            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
+                BOX
+            </div>
+        </motion.div>
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+            variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 1, y: 0 }
+            }}
+            className="m-3"
+        >
+            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
+                BOX
+            </div>
+        </motion.div>
+    </div>
+    <div className="flex flex-wrap items-center justify-center">
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+            variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 1, y: 0 }
+            }}
+            className="m-3"
+        >
+            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
+                BOX
+            </div>
+        </motion.div>
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+            variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 1, y: 0 }
+            }}
+            className="m-3"
+        >
+            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
+                BOX
+            </div>
+        </motion.div>
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+            variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 1, y: 0 }
+            }}
+            className="m-3"
+        >
+            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
+                BOX
+            </div>
+        </motion.div>
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+            variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 1, y: 0 }
+            }}
+            className="m-3"
+        >
+            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
+                BOX
+            </div>
+        </motion.div>
+    </div>
     <div className="flex flex-col items-center justify-between">
-        <motion.div
+    <motion.div
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true }}
@@ -100,8 +287,8 @@ export default function Hero() {
             viewport={{ once: true }}
             transition={{ duration: 1 }}
             variants={{
-                hidden: { opacity: 0, y: 100 },
-                visible: { opacity: 1, y: 0 }
+                hidden: { y: 100 },
+                visible: { y: 0 }
             }}
             className="mb-3"
         >
@@ -115,68 +302,8 @@ export default function Hero() {
             viewport={{ once: true }}
             transition={{ duration: 1 }}
             variants={{
-                hidden: { opacity: 0, y: 100 },
-                visible: { opacity: 1, y: 0 }
-            }}
-            className="mb-3"
-        >
-            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
-                BOX
-            </div>
-        </motion.div>
-        <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            transition={{ duration: 1 }}
-            variants={{
-                hidden: { opacity: 0, y: 100 },
-                visible: { opacity: 1, y: 0 }
-            }}
-            className="mb-3"
-        >
-            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
-                BOX
-            </div>
-        </motion.div>
-        <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            transition={{ duration: 1 }}
-            variants={{
-                hidden: { opacity: 0, y: 100 },
-                visible: { opacity: 1, y: 0 }
-            }}
-            className="mb-3"
-        >
-            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
-                BOX
-            </div>
-        </motion.div>
-        <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            transition={{ duration: 1 }}
-            variants={{
-                hidden: { opacity: 0, y: 100 },
-                visible: { opacity: 1, y: 0 }
-            }}
-            className="mb-3"
-        >
-            <div className="flex items-center justify-center rounded-md border-2 border-gray-900 bg-blue-500 w-64 h-64">
-                BOX
-            </div>
-        </motion.div>
-        <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-            transition={{ duration: 1 }}
-            variants={{
-                hidden: { opacity: 0, y: 100 },
-                visible: { opacity: 1, y: 0 }
+                hidden: { y: 100 },
+                visible: { y: 0 }
             }}
             className="mb-3"
         >
@@ -185,5 +312,6 @@ export default function Hero() {
             </div>
         </motion.div>
     </div>
+    </>
   );
 }
